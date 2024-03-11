6ac16466d6d1dacb6fd16b3c472cdd03996d663e
Jason <jtoy@grids.local>
2024-03-10 18:13:52 -0700

    more stats

--- utils/timeline_info.py
+++ utils/timeline_info.py
@@ -12,6 +12,8 @@ deaths = 0
 sim_id = ""
 scenario = ""
 world = ""
+last_death_at_step = 0
+last_death_at_row = 0
 with jsonlines.open(path, "r") as jsonl_file:
     for i, row in enumerate(jsonl_file):
         obj = json.loads(row)
@@ -27,6 +29,8 @@ with jsonlines.open(path, "r") as jsonl_file:
             completed = True
         elif "status" in obj and obj["status"] == "dead":
             deaths += 1
+            last_death_at_step = max_step
+            last_death_at_row = i+1
         elif step_type == "matrix_init":
             sim_id = obj["sim_id"]
             scenario = obj["data"]["scenario"]
@@ -43,6 +47,8 @@ print(f"step types {step_types}")
 print(f"average substeps per step {total_steps/max_step}")
 print(f"human agent count: {len(human_agents)}")
 print(f"deaths: {deaths}")
+print(f"last_death_at_step {last_death_at_step}")
+print(f"last_death_at_row {last_death_at_row}")
 #print(f"human agents {human_agents}")
 print(f"total agent count: {len(agents)}")
 #print(f"agent actions {agent_actions}")
